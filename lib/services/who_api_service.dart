06cf39254814fba6295e881592d6d60c972f8efc
unknown <katicheche2001@gmail.com>
2025-10-30 18:38:30 +0100

    changement d'url dans who_api

--- lib/services/who_api_service.dart
+++ lib/services/who_api_service.dart
@@ -108,7 +108,8 @@ class WHOApiService {
 
       // 🎯 MODIFICATION 1 : Utiliser l'indicateur plus fiable NCD_MORT_CANCER
       //  final url = '$_baseUrl/NCD_MORT_CANCER?\$filter=($countryFilter)';
-      final url = '$_baseUrl/NCDMORT3070?\$filter=($countryFilter) and Dim1 eq \'TOTL\' and Dim2 eq \'TOTL\'';
+      
+          final url = '$_baseUrl/NCDMORT3070?\$filter=($countryFilter)';
       print('Requête API WHO: $url');
 
       final response = await http
